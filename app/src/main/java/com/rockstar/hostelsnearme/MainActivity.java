2fcb8956a1a9b3d527288f96f6ebed74b7ef02f3
sivaprasad1999 <gsprasad789@gmail.com>
2021-04-29 12:34:50 +0530

    throught android studio

--- app/src/main/java/com/rockstar/hostelsnearme/MainActivity.java
+++ app/src/main/java/com/rockstar/hostelsnearme/MainActivity.java
@@ -1,5 +1,6 @@
 package com.rockstar.hostelsnearme;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,30 +10,53 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class MainActivity extends AppCompatActivity {
     EditText email,pass;
     SharedPreferences sp;
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        auth = FirebaseAuth.getInstance();
         email=findViewById(R.id.editTextmail);
         pass=findViewById(R.id.editTextPass);
+
+       /* if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(MainActivity.this, MainActivity.class));
+            finish();
+        }*/
     }
     public void Login(View view) {
         String emaill=email.getText().toString();
         String mypass=pass.getText().toString();
-        String nn=RegisterPage.spp.getString("Emaill","");
-        String pp=RegisterPage.spp.getString("Passwoard","");
+        /*String nn=RegisterPage..getString("Emaill","");
+        String pp=RegisterPage..getString("Passwoard","");
         if (nn.equals(emaill) && pp.equals(mypass)){
 //            Toast.makeText(this, "Login Success", Toast.LENGTH_SHORT).show();
         }
         else {
             Toast.makeText(this, "Login failed ", Toast.LENGTH_SHORT).show();
-        }
+        }*/
+
+
+        auth.signInWithEmailAndPassword(emaill, mypass).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        Toast.makeText(MainActivity.this, "Login Sucess", Toast.LENGTH_SHORT).show();
+
+                    }else{
+                        Toast.makeText(MainActivity.this, "Invalid User", Toast.LENGTH_SHORT).show();
+                    }
+            }
+        });
 
     }
     public void signup(View view) {
